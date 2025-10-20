c78ac8f8f394090df187a1b3e11e441a56b7b1f1
Praneeth5158 <161028719+Praneeth5158@users.noreply.github.com>
2025-10-20 12:52:59 +0530

    Add files via upload

---
+++ Pattern programs/ryt.java
@@ -0,0 +1,17 @@
+class ryt{
+    public static void main(String args[]){
+        int i,j,rows=5;
+    /*  *
+        * *
+        * * *
+        * * * *
+        * * * * *
+    */
+    for (i=0;i<rows;i++){
+        for (j=0;j<=i;j++){
+            System.out.print("* ");
+        }
+        System.out.print("\n");
+    }
+    }
+}
