8d4326114a0b2fb1e28a1eaf495ccb88a038c694
Praneeth5158 <161028719+Praneeth5158@users.noreply.github.com>
2025-10-20 12:52:02 +0530

    Add files via upload

---
+++ functions.java
@@ -0,0 +1,13 @@
+class functions {
+    public static void message (){
+        System.out.println("Hi iam using function");
+    }
+    public static void sum(int a,int b){
+        int sum=a+b;
+        System.out.println("vorey erripukaa nuvvu rendu numbers ichaav kada daani sum enti antey "+sum);
+    }
+    public static void main(String[] args){
+        message();
+        sum(5,10);
+    }
+}
