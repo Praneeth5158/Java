1c9686ac333feb233ecbc69ea4dc0be61ff994d5
Praneeth5158 <161028719+Praneeth5158@users.noreply.github.com>
2025-10-21 11:56:21 +0530

    Add files via upload

--- Final_keyword.java
+++ Final_keyword.java
@@ -1,5 +1,5 @@
 //--------final keyword means “fixed” — once you set it, you can’t change it.
-
+/*
 class final_ex{
     public final void fn1(){
         System.out.println("This method cannot be overriden by any sub class");
@@ -14,5 +14,21 @@ class Final_keyword{
     public static void main(String[] args){
         sub obj=new sub();
         obj.fn1();
+    }
+}
+
+*/
+
+class fn{
+    static final int a=10;
+    static void fn(){
+            a=30;//this will show an erro bcz we already mentioned above as final so final variable cannot able to change
+            System.out.println("My number is "+ a);
+    }
+}
+class Final_keyword{
+    public static void main(String[] args) {
+        fn.fn();
+
     }
 }
